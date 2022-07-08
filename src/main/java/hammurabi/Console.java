2c8949503052792885e6ddc6388431baa31c1fb4
Ryan Chudd <ryan.a.chudd@gmail.com>
2022-07-08 18:54:34 -0400

    class setup

---
+++ src/main/java/hammurabi/Console.java
@@ -0,0 +1,7 @@
+package hammurabi;
+
+public class Console {
+    public void go() {
+        System.out.println("Here we go!");
+    }
+}
--- src/main/java/hammurabi/Hammurabi.java
+++ src/main/java/hammurabi/Hammurabi.java
@@ -1,4 +1,11 @@
 package hammurabi;
 
 public class Hammurabi {
+    // Console class -- handle output/input to the user
+    // Calculations class
+
+    public static void main(String[] args) {
+        Console console = new Console();
+        console.go();
+    }
 }
