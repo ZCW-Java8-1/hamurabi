3428fb11852a3cf4bc1d4a26e5a440288b5296ce
octavia <Ogomes21@gmail.com>
2022-07-09 17:26:59 -0400

    Added while loop to loop through questions.

--- src/main/java/hammurabi/Console.java
+++ src/main/java/hammurabi/Console.java
@@ -15,13 +15,28 @@ public class Console {
 
 
     public void playGame() { // call methods in loop here?
-        summary();
-        askHowManyAcresToBuy();
-        askHowMuchGrainToFeedPeople();
-        askHowManyAcresToPlant();
+        instructions();
+
+        while(currentYear < 11) {
+            summary();
+            askHowManyAcresToBuy();
+            askHowMuchGrainToFeedPeople();
+            askHowManyAcresToPlant();
+            currentYear++;
+        }
+
+
     }
 
-
+    public void instructions() {
+        System.out.println("Congratulations, you are the newest ruler of ancient Sumer, elected for a ten year term of office. Your duties are to dispense food, direct farming, and buy and sell land as needed to support your people. Watch out for rat infestiations and the plague! Grain is the general currency, measured in bushels. The following will help you in your decisions:\n" +
+                "\n" +
+                "Each person needs at least 20 bushels of grain per year to survive\n" +
+                "Each person can farm at most 10 acres of land\n" +
+                "It takes 2 bushels of grain to farm an acre of land\n" +
+                "The market price for land fluctuates yearly\n" +
+                "Rule wisely and you will be showered with appreciation at the end of your term. Rule poorly and you will be kicked out of office!");
+    }
     public void summary() {
         System.out.println("O great Hammurabi!\n" +
                 "You are in year " + currentYear + " of your ten year rule.\n" +
