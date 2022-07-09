64c92803b2e339e0c4cb33bc70499526d05dd94c
octavia <Ogomes21@gmail.com>
2022-07-09 12:33:06 -0400

    Moved some things around.

--- src/main/java/hammurabi/Console.java
+++ src/main/java/hammurabi/Console.java
@@ -5,29 +5,31 @@ import java.util.Scanner;
 
 public class Console {
     Random rand = new Random();
+    int currentYear = 1;
+    int acres = 1000;
+    int bushels = 2800;
+    int population = 100;
+    int landValue = 19;
 
     public void playGame() {
-        int acres = 0;
-        int grain = 0;
-        int population = 0;
+        summary();
 
-        //initial state summary
-        System.out.println("O great Hammurabi!\n" +
-                "You are in year 1 of your ten year rule.\n" +
-                "In the previous year 0 people starved to death.\n" +
-                "In the previous year 5 people entered the kingdom.\n" +
-                "The population is now 100.\n" +
-                "We harvested 3000 bushels at 3 bushels per acre.\n" +
-                "Rats destroyed 200 bushels, leaving 2800 bushels in storage.\n" +
-                "The city owns 1000 acres of land.\n" +
-                "Land is currently worth 19 bushels per acre.");
         // call methods in loop here?
+
+
+    }
+    public void summary() {
+        System.out.println("O great Hammurabi!\n" +
+                "You are in year "+currentYear+" of your ten year rule.\n" +
+                //"In the previous year 0 people starved to death.\n" +
+               //"In the previous year 5 people entered the kingdom.\n" +
+                "The population is now "+population+".\n" +
+                "We harvested "+bushels+" bushels at 3 bushels per acre.\n" +
+               //"Rats destroyed 200 bushels, leaving 2800 bushels in storage.\n" +
+                "The city owns "+acres+" acres of land.\n"); //delete once next line is needed
+                //"Land is currently worth "+landValue+" bushels per acre.");
 
         System.out.println ("How many acres of land do you wish to buy?");
         Scanner scan = new Scanner(System.in); //Create a scanner object
-         = scan.nextLine(); //Read user input
-
-
-
     }
 }
