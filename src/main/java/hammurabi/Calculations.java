9b8903b39b6b8f228f1b5f2a7f3e06e2522c2e4d
Ryan Chudd <ryan.a.chudd@gmail.com>
2022-07-09 09:50:36 -0400

    setup notes

--- src/main/java/hammurabi/Calculations.java
+++ src/main/java/hammurabi/Calculations.java
@@ -1,4 +1,6 @@
 package hammurabi;
 
 public class Calculations {
+    // calls from Console class come here to these methods
+    // parameters passed in and answers returned to Console for printing results
 }
