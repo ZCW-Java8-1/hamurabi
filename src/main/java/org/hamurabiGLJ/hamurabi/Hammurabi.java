befb0bdf07de70b2d8c5336266000080ad45d555
Jiayong <jiagains@gmail.com>
2022-07-08 23:18:01 -0400

    add skeleton methods

--- src/main/java/org/hamurabiGLJ/hamurabi/Hammurabi.java
+++ src/main/java/org/hamurabiGLJ/hamurabi/Hammurabi.java
@@ -15,5 +15,36 @@ public class Hammurabi {
         // statements go after the declations
     }
 
+    int getNumber(String message) {
+        while (true) {
+            System.out.print(message);
+            try {
+                return scanner.nextInt();
+            }
+            catch (InputMismatchException e) {
+                System.out.println("\"" + scanner.next() + "\" isn't a number!");
+            }
+        }
+    }
+    int askHowManyAcresToBuy(int price, int bushels) {return 0;}
     //other methods go here
+    int askHowManyAcresToSell(int acresOwned) {return 0;}
+
+    int askHowMuchGrainToFeedPeople(int bushels) {return 0;}
+
+    int askHowManyAcresToPlant(int acresOwned, int population, int bushels) {return 0;};
+
+    int plagueDeaths(int population) {return 0;}
+
+    int starvationDeaths(int population, int bushelsFedToPeople) {return 0;}
+
+    boolean uprising(int population, int howManyPeopleStarved) {return false;}
+
+    int immigrants(int population, int acresOwned, int grainInStorage) {return 0;}
+
+    int harvest(int acres, int bushelsUsedAsSeed) {return 0;}
+
+    int grainEatenByRats(int bushels) {return 0;}
+
+    int newCostOfLand() {return 0;}
 }
