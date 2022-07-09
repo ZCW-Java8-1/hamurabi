97c54a95dac2ee7313af7b5500ff4d205458dc01
Jiayong <jiagains@gmail.com>
2022-07-08 23:05:52 -0400

    add skeleton

--- src/main/java/org/hamurabiGLJ/hamurabi/Hammurabi.java
+++ src/main/java/org/hamurabiGLJ/hamurabi/Hammurabi.java
@@ -1,6 +1,19 @@
 package org.hamurabiGLJ.hamurabi;
 
+import java.util.*;
+
 public class Hammurabi {
+    Random rand = new Random();  // this is an instance variable
+    Scanner scanner = new Scanner(System.in);
 
+    public static void main(String[] args) { // required in every Java program
+        new Hammurabi().playGame();
+    }
 
+    void playGame() {
+        // declare local variables here: grain, population, etc.
+        // statements go after the declations
+    }
+
+    //other methods go here
 }
