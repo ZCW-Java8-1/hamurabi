1fa4baaad4dac14453bbf0b07a50edd0da4c25a9
octavia <Ogomes21@gmail.com>
2022-07-09 10:41:11 -0400

    Added deaths.

--- src/main/java/hammurabi/Calculations.java
+++ src/main/java/hammurabi/Calculations.java
@@ -3,4 +3,5 @@ package hammurabi;
 public class Calculations {
     // calls from Console class come here to these methods
     // parameters passed in and answers returned to Console for printing results
+    // deaths
 }
