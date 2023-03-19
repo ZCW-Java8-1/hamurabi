127f5c4d0020266d19defa3bdc202a8022982d98
deanOfWalls <deanofwalls@gmail.com>
2023-03-19 01:43:04 -0400

    checkpointing my work

--- src/main/java/Hammurabi.java
+++ src/main/java/Hammurabi.java
@@ -1,11 +1,12 @@
 import java.util.Random;
 import java.util.Scanner;
+import java.lang.Math;
 
 public class Hammurabi {
 
     int plagueDeaths = 0;
     int starvationDeaths = 0;
-    int uprising = 0;
+    boolean uprising = false;
     int immigrants = 0;
     int harvest = 0;
     int grainEatenByRats = 0;
@@ -14,50 +15,69 @@ public class Hammurabi {
     int population = 100;
     int acres = 1000;
     int bushels = 2800;
-    public int plagueDeaths(int i) {
+    boolean isAlive = true;
+    Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) { // required in every Java program
+        new Hammurabi().playGame();
+    }
+
+    void playGame() {
+        // declare local variables here: grain, population, etc.
+        // statements go after the declarations
+        System.out.println("Welcome, great Hammurabi! You have been chosen to govern the people " + "for the next " + yearsLeft + " years.\nBefore you begin your reign please consider the following: " + "Your starting population is " + population + "\nYour starting land is " + acres + " acres.\nYour starting" + " grain reserves are " + bushels + " bushels.\nThe current value of land is " + newCostOfLand + " bushels/acre");
+
+
+        int deaths = plagueDeaths(this.population);
+
+//       while (isAlive) {
+//            if (populationThisTurn < 45 % of populace){
+//                isAlive = false;
+//                break;
+//            }
+//        }
+        System.out.println(deaths);
+    }
+
+
+    public int plagueDeaths(int population) {
+        if(Math.random() * 100 <=15){
+            this.population = population / 2;
+        }
+        return population - this.population;
+    }
+
+
+
+    public int starvationDeaths(int population, int bushelsFedToPeople) {
+
         return 0;
     }
-    public int starvationDeaths(int i) {
-        return 0;
-    }
+
     public int uprising(int i) {
+
         return 0;
     }
+
     public int immigrants(int i) {
+
         return 0;
     }
+
     public int harvest(int i) {
+
         return 0;
     }
+
     public int grainEatenByRats(int i) {
+
         return 0;
     }
+
     public int newCostOfLand(int i) {
+
         return 0;
     }
-        Random rand = new Random();  // this is an instance variable
-        Scanner scanner = new Scanner(System.in);
-
-        public static void main(String[]args) { // required in every Java program
-            new Hammurabi().playGame();
-
-
-
-        }
-
-        void playGame() {
-            // declare local variables here: grain, population, etc.
-            // statements go after the declarations
-            System.out.println("Welcome, great Hammurabi! You have been chosen to govern the people " +
-                    "for the next "+ yearsLeft + " years.\nBefore you begin your reign please consider the following: "+
-                    "Your starting population is " + population + "\nYour starting land is " + acres + " acres.\nYour starting" +
-                    " grain reserves are " + bushels + " bushels.\nThe current value of land is " + newCostOfLand + " bushels/acre");
-
-
-
-
-        }
-
 
 
 }
